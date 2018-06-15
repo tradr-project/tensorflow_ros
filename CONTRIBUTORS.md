4136cde2e3afa8df957e1ef6f5ca6d158ce12bb4
Martin Pecka <peckama2@fel.cvut.cz>
2018-06-15 15:29:22 +0200

    Redirecting to tensorflow_ros_cpp

--- CONTRIBUTORS.md
+++
@@ -1,2 +0,0 @@
-- [Martin Pecka](https://github.com/peci1)
-- [Isaac I.Y. Saito](https://github.com/130s)
--- scripts/detect_cxx11_abi.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-COMPILER="$1"
-echo "#include <string>" | ${COMPILER} -x c++ -E -dM - | fgrep _GLIBCXX_USE_CXX11_ABI
